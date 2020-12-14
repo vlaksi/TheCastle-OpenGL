b6ec711a2c2d96495b17a2fc8fca999dbf860a67
vlaksi <daksrapsody@gmail.com>
2020-12-14 20:32:02 +0100

    [refactor]: Reformatiranje

--- AssimpSample/AssimpSample/World.cs
+++ AssimpSample/AssimpSample/World.cs
@@ -206,8 +206,6 @@ namespace AssimpSample
             OsnovneInteraktivneTransformacije(gl);
             SkaliranjeCitaveScene(gl);
 
-            
-
             ManipulacijaPodlogom(gl);
             ManipulacijaStrelom(gl);
             ManipulacijaDvorcem(gl);
